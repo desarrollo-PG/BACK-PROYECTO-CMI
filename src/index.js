eb37202455df52f3ce7f374cf37ab170d947584e
Jerber <jcintor@miumg.edu.gt>
2025-09-10 23:30:47 -0600

    cambios en el idex para desplegar en production

--- src/index.js
+++ src/index.js
@@ -13,10 +13,20 @@ const app = express();
 // Seguridad
 app.use(helmet());
 
+// CORS ANTIGUOS
+
+//app.use(cors({
+//  origin: process.env.FRONTEND_URL,
+//  credentials: true
+//}));
+
 // CORS
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  credentials: true
+  origin: '*',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 // Logging
