e53c1cb33b82a84f739e67567acabea3d180b718
Jerber <jcintor@miumg.edu.gt>
2025-09-14 10:43:39 -0600

    cambios para levantar index.js en railway

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ app.use(helmet());
 // CORS
 
 app.use(cors({
-  origin: 'true',
+  origin: true,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
